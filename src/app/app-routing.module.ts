ee5689e07dc110eee575842665f2b7f232983eff
Giovanni230201 <ilgenius234@gmail.com>
2023-04-13 11:32:12 +0200

    creato il modulo delle ricette

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
-import { RecipesComponent } from './components/recipes/recipes.component';
-import { RecipesListComponent } from './components/recipes/recipes-list/recipes-list.component';
-import { DetailComponent } from './components/recipes/detail/detail.component';
-import { NewRecipeComponent } from './components/new-recipe/new-recipe.component';
 import { EsempioCombineComponent } from './components/esempio-combine/esempio-combine.component';
 import { LoginComponent } from './components/user/login/login.component';
 import { ProfileComponent } from './components/user/profile/profile.component';
@@ -15,15 +11,11 @@ import { LoggedInGuard } from './logged-in.guard';
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home',component: HomeComponent},
-  {path:'ricette',component: RecipesComponent, children:[
-    {path:'dettaglio/:title/:_id', component: DetailComponent},
-    {path:'', pathMatch: 'full', component:RecipesListComponent}
-  ] },
   {path:'sign-in', component: SignInComponent},
   {path:'login', component: LoginComponent},
   {path:'profile', component: ProfileComponent,canActivate: [LoggedInGuard]},
-  {path:'new-recipe', component: NewRecipeComponent},
   {path:'combine', component: EsempioCombineComponent},
+  {path: 'recipes', loadChildren: () => import("./components/recipes/recipes.module").then(modulo => modulo.RecipesModule)},
   {path:'**',redirectTo: 'home'}
 ];
 
